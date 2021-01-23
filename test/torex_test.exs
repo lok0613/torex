c78178250886058838b3bf88f511745e53769705
Lok Chun Wai <cwlok0613@gmail.com>
2021-01-23 20:42:35 +0800

    Add test case

--- test/torex_test.exs
+++ test/torex_test.exs
@@ -1,8 +1,22 @@
 defmodule TorexTest do
   use ExUnit.Case
-  doctest Torex
+  require Logger
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Torex is not using my own ip" do
+    tor_ip = "https://api.myip.com"
+    |> Torex.get!()
+    |> Poison.decode!()
+    |> Map.get("ip")
+
+    my_ip = "https://api.myip.com"
+    |> HTTPoison.get!()
+    |> Map.get(:body)
+    |> Poison.decode!()
+    |> Map.get("ip")
+
+    Logger.info("Tor IP - #{tor_ip}")
+    Logger.info("My IP - #{my_ip}")
+
+    assert tor_ip !== my_ip
   end
 end
